0b4d34bca77b9d1c7edebd6acca5d6ccfeca34ef
Abdelrahman Atef <78609537+Abdo99-alt@users.noreply.github.com>
2024-01-03 23:35:15 +0200

    Add files via upload

---
+++ rtl/main_decoder.v
@@ -0,0 +1,25 @@
+//Input: opcode (Instr[6:0])
+//Output: RegWrite, ImmSrc, ALUSrc, Branch, MemWrite, ResultSrc, ALUOp, Jump
+module main_decoder (
+    opcode, RegWrite, ImmSrc, ALUSrc, Branch, MemWrite, ResultSrc, ALUOp ,Jump  
+);
+    input [6:0] opcode;
+    output wire RegWrite, ALUSrc, Branch, MemWrite, ResultSrc, Jump;
+    output wire [1:0] ImmSrc, ALUOp;
+    /////////////////////////////////////////////////////////////
+    reg [9:0] controls;
+    /////////////////////////////////////////////////////////////
+    assign {RegWrite, ImmSrc, ALUSrc, Branch, MemWrite, ResultSrc, ALUOp, Jump} = controls;
+    always @(opcode) begin
+        case (opcode)
+            7'b0000011: controls = 10'b1_00_1_0_0_1_00_0;  //LW
+            7'b0100011: controls = 10'b0_01_1_0_1_0_00_0;  //SW
+            7'b1100011: controls = 10'b0_10_0_1_0_0_01_0;  //BEQ & BNE
+            7'b0110011: controls = 10'b1_00_0_0_0_0_10_0;  //R-Type
+            7'b0010011: controls = 10'b1_00_1_0_0_0_10_0;  //ADDI & ORI & ANDI
+            7'b1101111: controls = 10'b1_11_0_0_0_0_00_1;  //JAL
+            7'b1100111: controls = 10'b1_00_1_0_0_0_00_1;  //JALR
+            default: controls = 10'bxxxxxxxxxx;
+        endcase 
+    end   
+endmodule
